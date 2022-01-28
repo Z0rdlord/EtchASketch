c42a92f7a91e2366f250dd7e7a5c4aa1c6980ff3
Kevin Guier <kevin.guier@gmail.com>
2022-01-27 21:09:58 -0600

    got the base of the grid working

--- jscript.js
+++ jscript.js
@@ -1,9 +1,17 @@
 
 const gridContainer = document.querySelector('div.gridContainer');
 
-for(let i= 0; i<16;i++){
+let rows = 16
+let cols = 16
+
+for(let i= 0; (i< rows * cols);i++){
 const divGridItem =document.createElement('div');
 divGridItem.setAttribute('class','gridItem');
 gridContainer.appendChild(divGridItem);
 }
 
+/*
+##########To Work on next #########
+create a change size button from 4-100
+the event listener to remove current element replace with new variables
+*/
